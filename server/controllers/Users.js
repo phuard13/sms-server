a0b90ad5cb1fe9c5ca0293c628f4cb33a897e702
fouhad <folalekan13@yahoo.com>
2020-07-21 17:00:33 +0100

    first committ

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -1,3 +1,5 @@
+
+
 import jwt from 'jsonwebtoken';
 import userModel from '../model/users';
 
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(BASE_URI, router);
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'app now live',
+    message: 'ap now live',
   });
 });
 
